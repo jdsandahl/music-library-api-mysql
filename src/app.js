94272b7a649f8f044bb38b2f3877d1ce5389376d
Justin Sandahl <sandahl.j.d@gmail.com>
2020-05-12 17:52:00 +0100

    added a controller to handled a POST request to /artists endpoint with the logic for a create() fuciton when said request has been made, this passes the unit test and also the end-to-end test using Postman.

--- src/app.js
+++ src/app.js
@@ -3,8 +3,12 @@ const express = require('express');
 const artistRouter = require('./routes/artist');
 const albumRouter = require('./routes/album');
 
+const artistController = require('./controllers/artists');
+
 const app = express();
 
 app.use(express.json());
+
+app.post('/artists',artistController.create);
 
 module.exports = app;
---
+++ src/controllers/artists.js
@@ -0,0 +1,7 @@
+/* src/controllers/artists.js */
+const { Artist } = require('../sequelize');
+
+exports.create = (req, res) => {
+  Artist.create(req.body).then(user => res.status(201).json(user));
+};
+
