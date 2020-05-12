57a5cda36e102e2e8d5dab83133600e58a7d8351
Justin Sandahl <sandahl.j.d@gmail.com>
2020-05-12 18:06:23 +0100

    move artist route to its own file and importing its use in app.js as artistRouter, the router file is the base for handling the different request routes to then be used by the necessary functions accessible to artistController that is importing the controller logic.

--- src/app.js
+++ src/app.js
@@ -3,12 +3,10 @@ const express = require('express');
 const artistRouter = require('./routes/artist');
 const albumRouter = require('./routes/album');
 
-const artistController = require('./controllers/artists');
-
 const app = express();
 
 app.use(express.json());
 
-app.post('/artists',artistController.create);
+app.use('/artists',artistRouter);
 
 module.exports = app;
---
+++ src/routes/artist.js
@@ -0,0 +1,10 @@
+/* src/routes/artist */
+
+const express = require('express');
+const artistController = require('../controllers/artists');
+
+const router = express.Router();
+
+router.post('/', artistController.create);
+
+module.exports = router;
