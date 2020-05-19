3ef548eac85ea10726c8015629e491a088896bb7
Justin Sandahl <sandahl.j.d@gmail.com>
2020-05-19 21:16:59 +0100

    First commit - this branch is the start of an alternate refactor using async/await. Refactored the createSong() controller to use async/await.

--- src/controllers/songs.js
+++ src/controllers/songs.js
@@ -2,23 +2,21 @@
 const { Album } = require('../sequelize');
 const { Song } = require('../sequelize');
 
-exports.createSong = (req, res) => {
+exports.createSong = async (req, res) => {
   const { albumId } = req.params;
 
-  Album.findByPk(albumId).then(album => {
-    if (!album) {
-      res.status(404).json({ error: 'The album could not be found'});
-    } else {
-      const songData = {
-        name: req.body.name,
-        albumId: album.id,
-        artistId: album.artistId,
-      };
-      Song.create(songData).then(song => {
-        res.status(201).json(song);
-      });
-    }
-  });
+  try {
+    const album = await Album.findByPk(albumId);
+    const songData = await {
+      name: req.body.name,
+      albumId: album.id,
+      artistId: album.artistId,
+    };
+    const song = await Song.create(songData);
+    res.status(201).json(song);
+  } catch { 
+    res.status(404).json({ error: 'The album could not be found '}); 
+  };
 };
 
 exports.getAlbumTracks = (req, res) => {
