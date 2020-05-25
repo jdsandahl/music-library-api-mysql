4866779e608b3715893c0ed3007844548721f219
Justin Sandahl <sandahl.j.d@gmail.com>
2020-05-25 20:54:44 +0100

    refactored the getAlbum tracks method to use the include syntax within the findAll() method.

--- src/controllers/songs.js
+++ src/controllers/songs.js
@@ -1,6 +1,5 @@
 /* src/controllers/songs */
-const { Album } = require('../sequelize');
-const { Song } = require('../sequelize');
+const { Album, Artist, Song } = require('../sequelize');
 
 exports.createSong = async (req, res) => {
   const { albumId } = req.params;
@@ -22,7 +21,19 @@ exports.getAlbumTracks = async (req, res) => {
   const { albumId } = req.params;
   try {
     const album = await Album.findByPk(albumId);
-    const songs = await Song.findAll({ where: { albumId: album.id } });
+    const songs = await Song.findAll({
+      include: [
+      {
+        model: Album,
+        as: 'album',
+        where: { id: album.id }
+      },
+      {
+        model: Artist,
+        as: 'artist'
+      }] 
+      });
+      
     res.status(200).json(songs);
   } catch {
     res.status(404).json({ error: 'The album could not be found.'});
