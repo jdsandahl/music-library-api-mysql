bc8f43765acdf4493e96a6da6d41284cdda112f6
Justin Sandahl <sandahl.j.d@gmail.com>
2020-05-25 20:37:56 +0100

    changed the getAlbumTracks method to use include syntax to provide additional information in the resonse data

--- src/controllers/songs.js
+++ src/controllers/songs.js
@@ -1,5 +1,5 @@
 /* src/controllers/songs */
-const { Album } = require('../sequelize');
+const { Album, Artist } = require('../sequelize');
 const { Song } = require('../sequelize');
 
 exports.createSong = (req, res) => {
@@ -28,7 +28,19 @@ exports.getAlbumTracks = (req, res) => {
     if(!song){
       res.status(404).json({ error: 'The album could not be found.'});
     } else {
-      Song.findAll({ where: { albumId } }).then(songs => res.status(200).json(songs));
+      Song.findAll({
+        include: [
+        {
+          model: Album,
+          as: 'album',
+          where: { id: albumId }
+        },
+        {
+          model: Artist,
+          as: 'artist'
+        }] 
+        })
+        .then(songs => res.status(200).json(songs));
     }
   });
 };
